dfa89aa26a2ffaf4d57ed53edb6ccd357f073ac0
Terry Pang <Terrypang89@hotmail.com>
2020-11-14 15:27:45 +0800

    add oanda to run1

--- run1.py
+++ run1.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 import backtrader as bt
+import samples.oandatest.oandatest as oandatest
 
 class SmaCross(bt.SignalStrategy):
     def __init__(self):
         sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
         crossover = bt.ind.CrossOver(sma1, sma2)
         self.signal_add(bt.SIGNAL_LONG, crossover)
+
 
 cerebro = bt.Cerebro()
 cerebro.addstrategy(SmaCross)
